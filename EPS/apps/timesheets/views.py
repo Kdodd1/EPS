0a5e115deb042471c7284640ce07525a68cdff11
Kyle Dodd <Kylepdodd1@gmail.com>
2018-11-20 11:59:21 -0500

    dashboard navbar

--- EPS/apps/timesheets/views.py
+++ EPS/apps/timesheets/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render, HttpResponse, redirect
 def index(request):
 
 	return render(request, "timesheets/index.html")
+
+def adminDash(request):
+
+	return render(request,"timesheets/adminDash.html")
