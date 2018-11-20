9dc1c58f20cea359820a7f19ac395abb56ffaa59
Kyle Dodd <Kylepdodd1@gmail.com>
2018-11-20 13:50:29 -0500

    add htmls

--- EPS/apps/timesheets/urls.py
+++ EPS/apps/timesheets/urls.py
@@ -3,5 +3,10 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.index),
-	url(r'^adminDash$', views.adminDash)
+	url(r'^adminDash$', views.adminDash),
+	url(r'^userDash$', views.userDash),
+	url(r'^dailyReports$', views.dailyReports),
+	url(r'^manage$', views.manage),
+	url(r'^reports$', views.reports),
+	url(r'^settings$', views.settings),
 ]
--- EPS/apps/timesheets/views.py
+++ EPS/apps/timesheets/views.py
@@ -7,3 +7,19 @@ def index(request):
 def adminDash(request):
 
 	return render(request,"timesheets/adminDash.html")
+
+def userDash(request):
+	return render(request, "timesheets/userDash.html")
+
+def dailyReports(request):
+	return render(request, "timesheets/dailyReports.html")
+
+def manage(request):
+	return render(request, "timesheets/manage.html")
+
+def reports(request):
+	return render(request, "timesheets/reports.html")
+
+def settings(request):
+	return render(request, "timesheets/settings.html")
+
