1ac38742e8a1943827800cff2ab8587d7e414dea
Kyle Dodd <Kylepdodd1@gmail.com>
2018-11-21 12:13:59 -0500

    Fixed html settings/manage

--- EPS/apps/timesheets/models.py
+++ EPS/apps/timesheets/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+	first_name = models.CharField(max_length=255)
+	last_name = models.CharField(max_length=255)
+	email= models.CharField(max_length=255)
+	password= models.CharField(max_length=255)
+	user_level = models.IntegerField()
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now_add=True)
+
+class Report(models.Model):
+	user = models.ForeignKey(User, related_name="reports")
+	task = models.CharField(max_length=85)
+	notes = models.TextField()
+	assist = models.TextField()
+	created_at = models.DateTimeField(auto_now_add = True)
+	updated_at = models.DateTimeField(auto_now = True)
+
+class Day(models.Model):
+	clock_in = models.DateTimeField(auto_now_add=True)
+	clock_out = models.DateTimeField()
+	date = models.DateField(auto_now_add=True)
+	report = models.OneToOneField(Report)
+	user = models.ForeignKey(User, related_name="days")
+	created_at = models.DateTimeField(auto_now_add = True)
+	updated_at = models.DateTimeField(auto_now = True)
